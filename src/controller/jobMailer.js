6e16234b612d0ce7ac0a31682e4bbd8f9304211b
Rezi Hardiansyah <33001556+rezihardi@users.noreply.github.com>
2022-12-20 09:50:29 +0700

    moment

--- src/controller/jobMailer.js
+++ src/controller/jobMailer.js
@@ -2,11 +2,11 @@ const controller = {}
 const cron = require('node-cron')
 const util = require('../helpers/util')
 const mailService = require('../service/mailer-service')
+const moment = require("moment");
 
-
-// cron.schedule('* * * * *', async () => {
-//     // console.log("Task is running every minute " + new Date())
-//     await mailService.sendEmail("email was sent to u")
-// });
+cron.schedule('* * * * *', async () => {
+    console.log("Task is running every minute " + moment().locale('id').format('LLL'))
+    // await mailService.sendEmail("email was sent to u")
+});
 
 module.exports = controller
